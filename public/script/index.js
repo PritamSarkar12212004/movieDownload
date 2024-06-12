c978ab4be806b2572cb9d8abf3d0fea9123f5c3b
Pritam Sarkar <pritam7796419792@gmail.com>
2024-06-12 13:25:00 -0700

    all set

--- public/script/index.js
+++ public/script/index.js
@@ -1,19 +1,25 @@
 const hamBar = document.querySelector(".hamBar");
 const SideBar = document.querySelector(".SideBar");
 const itemsHi = document.querySelector(".itemsHi");
-
 const pcLOgbtn = document.querySelector(".pcLOgbtn");
 const pcLogin = document.querySelector(".pcLogin");
+const sideImg=document.querySelector(".sideImg")
+const footer=document.querySelector(".footer")
 
 flag = true;
 hamBar.addEventListener("click", () => {
   if (flag == true) {
     SideBar.classList.remove("hidden");
+    sideImg.classList.add("hidden");
     itemsHi.classList.add("hidden");
+    footer.classList.add("hidden");
     flag = false;
   } else {
     SideBar.classList.add("hidden");
+    sideImg.classList.remove("hidden");
     itemsHi.classList.remove("hidden");
+    footer.classList.remove("hidden");
+
     flag = true;
   }
 });
--- public/script/show.js
+++ public/script/show.js
@@ -1,16 +1,27 @@
 const hamBar = document.querySelector(".hamBar");
 const SideBar = document.querySelector(".SideBar");
 const showmovie = document.querySelector(".showmovie");
+const sideImg=document.querySelector(".sideImg")
+const footer=document.querySelector(".footer")
+
 
 flag = true;
 hamBar.addEventListener("click", () => {
   if (flag == true) {
     SideBar.classList.remove("hidden");
     showmovie.classList.add("hidden");
+    footer.classList.add("hidden");
+
+
+    
     flag = false;
   } else {
+
+
     SideBar.classList.add("hidden");
     showmovie.classList.remove("hidden");
+    footer.classList.remove("hidden");
+
     flag = true;
   }
 });
