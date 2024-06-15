6b5d636405e0a984234e1a865ac8279235d7b05e
Pritam Sarkar <pritam7796419792@gmail.com>
2024-06-15 11:26:55 -0700

    allset

--- controller/search.js
+++ controller/search.js
@@ -1,5 +1,14 @@
-const search = (req,res) => {
-    console.log(req.query)
+const movieupload = require("../model/movieupload");
+const search = async (req, res) => {
+    let Input = await req.query.inputSearch;
+    let data = await movieupload.find({ Movie: Input });
+
+    if (data.length === 0) {
+      res.render("./error/pageNotFound.ejs")
+    } else {
+       res.render("filter.ejs",{data})
+    }
+   
 }
 
 module.exports = search;
