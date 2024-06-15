a8fd4f7ffc18c9c34124431e6afc4f3f8b066824
Pritam Sarkar <pritam7796419792@gmail.com>
2024-06-15 02:42:48 -0700

    all set

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,17 +1,8 @@
 const movieupload = require("../model/movieupload.js");
 
-let midData = [];
 const home = async (req, res) => {
-  try {
-    const preData = await movieupload.find({}, "tags");
-    midData = preData.map((item) => item.tags).flat();
-  } catch (error) {
-    res.send("no data is present");
-  }
-
-  let data = await movieupload.find();
+  let data = await movieupload.find()
   res.render("./index.ejs", { data });
 };
-console.log(midData);
 
-module.exports = { home, midData };
+module.exports = { home };
