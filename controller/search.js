f97064518dd39b6d9eafc690f27bb3f06c7329d0
Pritam Sarkar <pritam7796419792@gmail.com>
2024-06-16 01:06:30 -0700

    all set

--- controller/search.js
+++ controller/search.js
@@ -1,14 +1,18 @@
 const movieupload = require("../model/movieupload");
-const search = async (req, res) => {
-    let Input = await req.query.inputSearch;
-    let data = await movieupload.find({ Movie: Input });
 
-    if (data.length === 0) {
-      res.render("./error/pageNotFound.ejs")
-    } else {
-       res.render("filter.ejs",{data})
-    }
-   
-}
+const search = async (req, res) => {
+  let input = req.query.inputSearch;
+
+  // Create a case-insensitive regular expression
+  let regex = new RegExp(input, "i");
+
+  let data = await movieupload.find({ Movie: regex });
+
+  if (data.length === 0) {
+    res.render("./error/pageNotFound.ejs");
+  } else {
+    res.render("filter.ejs", { data });
+  }
+};
 
 module.exports = search;
