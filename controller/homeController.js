ec1a980a804f746478f7982b0c3828edca2e5eea
PritamSarkar12212004 <162453679+PritamSarkar12212004@users.noreply.github.com>
2024-06-26 18:20:55 +0530

    Update homeController.js
    sort

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,7 +1,7 @@
 const movieupload = require("../model/movieupload.js");
 
 const home = async (req, res) => {
-  let data = await movieupload.find().sort({ headline: -1 })
+  let data = await movieupload.find().sort({ headline: 1 })
   res.render("./index.ejs", { data });
 };
 
